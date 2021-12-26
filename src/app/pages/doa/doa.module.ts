999449ea5c8d949dac372c6e2e2c44b8da0a90f8
adi <adi.sunjana@redtech.co.id>
2021-12-26 13:14:02 +0700

    add page

---
+++ src/app/pages/doa/doa.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DoaRoutingModule } from './doa-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DoaRoutingModule
+  ]
+})
+export class DoaModule { }
---
+++ src/app/pages/read-quran/read-quran.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReadQuranRoutingModule } from './read-quran-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReadQuranRoutingModule
+  ]
+})
+export class ReadQuranModule { }
